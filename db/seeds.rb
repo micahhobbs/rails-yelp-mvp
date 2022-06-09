4a6099bd936d2a8029fdba07b174f54bcbea928b
Micah Hobbs <micahthobbs@gmail.com>
2022-06-09 19:53:09 +1000

    Yelp MVP complete

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts 'Cleaning database...'
+Restaurant.destroy_all
+
+puts 'Creating restaurants...'
+dishoom = { name: 'Dishoom', address: '7 Boundary St, London E2 7JE', phone_number: 0400452333, category: 'italian' }
+pizza_east = { name: 'Pizza East', address: '56A Shoreditch High St, Long E1 6PQ', phone_number: 0417211441, category: 'chinese' }
+pizza_north = { name: 'Pizza North', address: '238 Shoreditch High St, Long E1 6PQ', phone_number: 12345, category: 'french' }
+pizza_south = { name: 'Pizza South', address: '128 Shoreditch High St, Long E1 6PQ', phone_number: 54321, category: 'japanese' }
+pizza_west = { name: 'Pizza West', address: '189 Shoreditch High St, Long E1 6PQ', phone_number: 0400452333, category: 'french' }
+
+[dishoom, pizza_east, pizza_north, pizza_south, pizza_west].each do |attributes|
+  restaurant = Restaurant.create!(attributes)
+  puts "Created #{restaurant.name}"
+end
+
+puts 'Finished'
